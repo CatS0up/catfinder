7b6e768b3912798edc97b98a8286ff1c031fb7d8
Damian <dwoz137@gmail.com>
2024-01-04 18:58:29 +0100

    Replace old logout HTML code with responsive-nav-link-logout blade component
    Replaced the old logout HTML code with a responsive-nav-link-logout blade component in the settingsOptions section of the responsive-menu blade component

--- resources/views/user/partials/navigation.blade.php
+++ resources/views/user/partials/navigation.blade.php
@@ -129,16 +129,7 @@
                 {{ __('Admin Panel') }}
                 </x-responsive-nav-link>
 
-                <div class="mt-3 space-y-1">
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('auth.logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('auth.logout')" onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
+                <x-responsive-nav-link-logout/>
             </x-slot>
         </x-responsive-menu>
     </x-slot>
