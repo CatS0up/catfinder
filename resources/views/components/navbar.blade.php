a13fb0b42e17c946d32bbb52e84b192d32258f8f
Damian <dwoz137@gmail.com>
2024-01-04 18:28:36 +0100

    Create navbar blade component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,48 @@
+@props([
+    'logoHref' => '#',
+])
+
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 dark:bg-gray-800 dark:border-gray-700">
+    <!-- Primary Navigation Menu -->
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="flex items-center shrink-0">
+                    <a href="{{ $logoHref }}">
+                        <x-application-logo class="block w-auto text-gray-800 fill-current h-9 dark:text-gray-200" />
+                    </a>
+                </div>
+
+                @isset ($navLinks)
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        {{ $navLinks }}
+                    </div>
+                @endisset
+            </div>
+
+            <!-- Settings Dropdown -->
+            @isset ($dropdowns)
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    {{ $dropdowns }}
+                </div>
+            @endisset
+
+            @isset($responsiveNavigationMenus)
+            <!-- Hamburger -->
+            <div class="flex items-center -me-2 sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400">
+                    <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Responsive Navigation Menus -->
+    {{ $responsiveNavigationMenus }}
+    @endisset
+</nav>
