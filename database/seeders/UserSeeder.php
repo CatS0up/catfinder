f074217ce027079fb7e6af1bc73b33e3ceecf09d
Damian <dwoz137@gmail.com>
2024-01-05 03:21:59 +0100

    Create UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'user',
+            'email' => 'user@user.com',
+            'password' => 'user',
+        ])
+        ->syncRoles('user');
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => 'admin',
+        ])
+        ->syncRoles('admin');
+    }
+}
