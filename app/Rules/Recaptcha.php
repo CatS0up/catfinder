c729146cfc49e348922a8fd4cad0c15cee71fc4f
Damian <dwoz137@gmail.com>
2024-01-05 03:38:44 +0100

    Create recaptcha validation rule

---
+++ app/Rules/Recaptcha.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Http;
+
+class Recaptcha implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $response = Http::asForm()->post(
+            url: 'https://www.google.com/recaptcha/api/siteverify',
+            data: [
+                'secret' => config('services.recaptcha.secret_key'),
+                'response' => $value,
+                'remoteip' => request()->ip(),
+            ],
+        );
+
+        if ( ! ($response->successful() && $response->json('success') && $response->json('score') > config('services.recaptcha.min_score'))) {
+            $fail('Failed to validate ReCaptcha');
+        }
+    }
+}
