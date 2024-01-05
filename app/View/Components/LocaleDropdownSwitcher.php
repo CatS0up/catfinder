af5b527abf4e5ae7ec72ac00c4fd01f3bb9291c7
Damian <dwoz137@gmail.com>
2024-01-05 02:36:23 +0100

    Add HasLocaleInfo trait to LocaleDropdownSwitcher and ResponsiveLocaleSwitcher components

--- app/View/Components/LocaleDropdownSwitcher.php
+++ app/View/Components/LocaleDropdownSwitcher.php
@@ -5,28 +5,21 @@ declare(strict_types=1);
 namespace App\View\Components;
 
 use Closure;
+use HasLocaleInfo;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class LocaleDropdownSwitcher extends Component
 {
+    use HasLocaleInfo;
     /**
      * Get the view / contents that represent the component.
      */
     public function render(): View|Closure|string
     {
-        $currentLocaleCode = app()->getLocale();
-        /** @var array<string, string> */
-        $availableLocales = config('app.available_locales');
-        $currentLocaleName = data_get(
-            target: array_flip($availableLocales),
-            key: $currentLocaleCode,
-            default: config('app.fallback_locale'),
-        );
-
         return view(
             view: 'components.locale-dropdown-switcher',
-            data: compact('currentLocaleCode', 'currentLocaleName', 'availableLocales')
+            data: $this->getLocaleInfo(),
         );
     }
 }
