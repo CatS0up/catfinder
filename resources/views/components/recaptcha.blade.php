2ff8bd48fe6f18f22a5d020da6bcc7b570c10e84
Damian <dwoz137@gmail.com>
2024-01-05 03:38:25 +0100

    Create recaptcha blade component

---
+++ resources/views/components/recaptcha.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'id' => 'recaptcha',
+    'name' => 'recaptcha',
+    'action' => 'register',
+])
+
+<p x-data x-init="$nextTick(() => {
+        const parentForm = $refs.input.closest('form');
+
+        if (! parentForm)
+        {
+            throw new Error('The given input does not have a parent form');
+        }
+
+        parentForm.addEventListener('submit', e => {
+            e.preventDefault();
+            grecaptcha.ready(() => {
+                grecaptcha.execute('{{ config('services.recaptcha.site_key') }}', { action: @js($action) })
+                .then(token => {
+                    $refs.input.value = token;
+                });
+            });
+        });
+     })">
+    <input x-ref="input" id="{{ $id }}" name="{{ $name }}" type="hidden">
+</p>
+
+@once
+    @push('scripts')
+        <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}" async defer></script>
+    @endpush
+@endonce
