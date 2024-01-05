90c68f6520dbd48d38c944683e6e8d7241036f01
Damian <dwoz137@gmail.com>
2024-01-05 09:45:32 +0100

    Create CatData data object

---
+++ app/DataObjects/CatData.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataObjects;
+
+use Spatie\LaravelData\Data;
+use App\Enums\CatGender;
+use App\Enums\CatStatus;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\EnumCast;
+
+final class CatData extends Data
+{
+    public function __construct(
+        public readonly ?int $id = null,
+        public readonly string $image_url,
+        public readonly string $name,
+        public readonly int $age,
+        public readonly string $race,
+        #[WithCast(EnumCast::class)]
+        public readonly CatGender $gender,
+        #[WithCast(EnumCast::class)]
+        public readonly ?CatStatus $status = null,
+        public readonly string $description,
+    ) {
+    }
+}
