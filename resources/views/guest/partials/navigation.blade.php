122ea25ac2dfb8eeb0ded1ab62dc6c778bb9d2ee
Damian <dwoz137@gmail.com>
2024-01-05 01:49:07 +0100

    Add locale-dropdown-switcher component to the user and guest layouts

--- resources/views/guest/partials/navigation.blade.php
+++ resources/views/guest/partials/navigation.blade.php
@@ -1,37 +1,6 @@
 <x-navbar :logoHref="route('guest.welcome')">
     <x-slot name="dropdowns">
-        <x-dropdown align="right" width="48">
-            <x-slot name="trigger">
-                <button
-                    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
-                    <div class="flex items-center">
-                        <img src="{{ asset('images/locale/pl.png') }}" alt="The icon for the Polish language displays the flag of Poland" class="w-4">
-                        <span class="ml-2">{{ __('PL') }}</span>
-                    </div>
-
-                    <div class="ms-1">
-                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20">
-                            <path fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                </button>
-            </x-slot>
-
-            <x-slot name="content">
-                <x-responsive-nav-link :href="route('user.profile.edit')" class="flex items-center">
-                    <img src="{{ asset('images/locale/pl.png') }}" alt="The icon for the Polish language displays the flag of Poland">
-                    <span class="ml-2">{{ __('PL') }}</span>
-                </x-responsive-nav-link>
-
-                <x-responsive-nav-link :href="route('admin.dashboard')" class="flex items-center">
-                    <img src="{{ asset('images/locale/en.png') }}" alt="The icon for the English language displays the flag of United Kingdom">
-                    <span class="ml-2">{{ __('EN') }}</span>
-                </x-responsive-nav-link>
-            </x-slot>
-        </x-dropdown>
+        <x-locale-dropdown-switcher/>
     </x-slot>
 
     <x-slot name="responsiveNavigationMenus">
