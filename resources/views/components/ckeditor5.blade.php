505b04c399c6f2e41bd9f32330dcedc7e3671436
Damian <dwoz137@gmail.com>
2024-01-05 09:44:25 +0100

    Create ckeditor5 blade component with custom file adapter

---
+++ resources/views/components/ckeditor5.blade.php
@@ -0,0 +1,106 @@
+@props([
+    'id' ,
+    'name',
+    'value' => '',
+])
+<div {{ $attributes }}>
+    <textarea
+        id="{{ $id }}"
+        name="{{ $name }}"
+        x-data
+        x-ref="input"
+        x-init="$nextTick(() => {
+                class CustomUploadAdapter
+                {
+                    constructor( loader ) {
+                        this.loader = loader;
+                    }
+
+                    upload() {
+                        return this.loader.file
+                            .then( file => new Promise( ( resolve, reject ) => {
+                                this._initRequest();
+                                this._initListeners( resolve, reject, file );
+                                this._sendRequest( file );
+                            } ) );
+                    }
+                    abort() {
+                        if ( this.xhr ) {
+                            this.xhr.abort();
+                        }
+                    }
+
+                    _initRequest() {
+                        const xhr = this.xhr = new XMLHttpRequest();
+                        xhr.open( 'POST', '{{ route('media.ckeditor.imageUpload') }}', true );
+                        xhr.setRequestHeader('x-csrf-token', '{{ csrf_token() }}');
+                        xhr.responseType = 'json';
+                    }
+
+                    _initListeners( resolve, reject, file ) {
+                        const xhr = this.xhr;
+                        const loader = this.loader;
+                        const genericErrorText = `Couldn't upload file: ${ file.name }.`;
+
+                        xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+                        xhr.addEventListener( 'abort', () => reject() );
+                        xhr.addEventListener( 'load', () => {
+                            const response = xhr.response;
+                            if ( !response || response.error ) {
+                                return reject( response && response.error ? response.error.message : genericErrorText );
+                            }
+
+                            resolve( {
+                                default: response.url
+                            } );
+                        } );
+
+                        if ( xhr.upload ) {
+                            xhr.upload.addEventListener( 'progress', evt => {
+                                if ( evt.lengthComputable ) {
+                                    loader.uploadTotal = evt.total;
+                                    loader.uploaded = evt.loaded;
+                                }
+                            } );
+                        }
+                    }
+
+                    _sendRequest( file ) {
+                        const data = new FormData();
+
+                        data.append( 'upload', file );
+
+                        this.xhr.send( data );
+                }
+            }
+
+                ClassicEditor
+                    .create($refs.input)
+                    .then(editor => {
+                        editor.plugins.get('FileRepository').createUploadAdapter = ( loader ) => {
+                            return new CustomUploadAdapter( loader );
+                        }
+
+                        editor.editing.view.change(writer => {
+                            writer.setStyle(
+                                'min-height',
+                                '200px',
+                                editor.editing.view.document.getRoot(),
+                            );
+                        });
+
+                        editor.setData(@js($value));
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+            })
+        ">
+    </textarea>
+</div>
+
+@push('scripts')
+    @once
+        @vite(['node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'])
+    @endonce
+@endpush
