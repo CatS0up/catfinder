749e7bb45a254b345aaf4ac246c6be7e878477fd
Damian <dwoz137@gmail.com>
2024-01-05 02:35:29 +0100

    Create HasLocaleInfo trait
    This trait includes a private method getLocaleInfo(), which returns information about the current locale setting of the application

---
+++ app/View/Concerns/HasLocaleInfo.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+trait HasLocaleInfo
+{
+    /** @return array<string, mixed> */
+    private function getLocaleInfo(): array
+    {
+        $currentLocaleCode = app()->getLocale();
+        /** @var array<string, string> */
+        $availableLocales = config('app.available_locales');
+        $currentLocaleName = data_get(
+            target: array_flip($availableLocales),
+            key: $currentLocaleCode,
+            default: config('app.fallback_locale'),
+        );
+
+        return compact('currentLocaleCode', 'currentLocaleName', 'availableLocales');
+    }
+}
