ec9a7c3fbfa4549a45e8a29f8708ab7b40ba9514
Damian <dwoz137@gmail.com>
2024-01-05 02:42:35 +0100

    Add responsive-locale-switcher component to the user and guest layouts

--- resources/views/user/partials/navigation.blade.php
+++ resources/views/user/partials/navigation.blade.php
@@ -51,29 +51,7 @@
             </x-slot>
         </x-responsive-menu>
 
-        <x-responsive-menu>
-            <x-slot name="settingsOptionsHeader">
-                <div class="flex items-center">
-                    <img src="{{ asset('images/locale/pl.png') }}"
-                        alt="The icon for the Polish language displays the flag of Poland" class="w-4">
-                    <span class="ml-2 text-base font-medium text-gray-800 dark:text-gray-200">{{ __('PL') }}</span>
-                </div>
-            </x-slot>
-
-            <x-slot name="settingsOptions">
-                <x-responsive-nav-link :href="route('user.profile.edit')" class="flex items-center mt-3 space-y-1">
-                    <img src="{{ asset('images/locale/pl.png') }}"
-                        alt="The icon for the Polish language displays the flag of Poland">
-                    <span class="ml-2">{{ __('PL') }}</span>
-                </x-responsive-nav-link>
-
-                <x-responsive-nav-link :href="route('admin.dashboard')" class="flex items-center mt-3 space-y-1">
-                    <img src="{{ asset('images/locale/en.png') }}"
-                        alt="The icon for the English language displays the flag of United Kingdom">
-                    <span class="ml-2">{{ __('EN') }}</span>
-                </x-responsive-nav-link>
-            </x-slot>
-        </x-responsive-menu>
+        <x-responsive-locale-switcher/>
 
         <x-responsive-menu>
             <x-slot name="settingsOptionsHeader">
