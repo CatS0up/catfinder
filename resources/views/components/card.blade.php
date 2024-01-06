c3921bc999713e6b51d734b078c26601818526dd
Damian <dwoz137@gmail.com>
2024-01-06 23:45:30 +0100

    Add text color classes to card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,4 @@
-<div {{ $attributes->class(['p-6', 'overflow-hidden', 'bg-white', 'shadow-md', 'dark:bg-gray-800', 'sm:rounded-lg']) }}>
+<div
+    {{ $attributes->class(['p-6', 'overflow-hidden', 'bg-white', 'shadow-md', 'dark:bg-gray-800', 'sm:rounded-lg', 'text-gray-900', 'sm:flex-row', 'dark:text-gray-100']) }}>
     {{ $slot }}
 </div>
