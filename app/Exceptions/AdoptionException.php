02e9ed755ca19d62bb96207ab18e9abb5b018a38
Damian <dwoz137@gmail.com>
2024-01-07 20:20:27 +0100

    Make AdoptionException renderable

--- app/Exceptions/AdoptionException.php
+++ app/Exceptions/AdoptionException.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 
 class AdoptionException extends Exception
 {
     public static function catHasInvalidStatus(): self
     {
         return new self('You can only adopt cats with the status of "available"');
+    }
+
+    public function render(): Response|RedirectResponse
+    {
+        return back()
+            ->with('warning', __('An error occurred while selecting a cat for adoption. Please try again later'));
     }
 }
