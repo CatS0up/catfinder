6b4bfb344a9860eff2a46ac55ef21f750fbbbc4f
Damian <dwoz137@gmail.com>
2024-01-08 00:10:32 +0100

    Fix CatAdoptionApprovalTest and add missing test cases

--- tests/Feature/Admin/CatAdoptionApprovalTest.php
+++ tests/Feature/Admin/CatAdoptionApprovalTest.php
@@ -3,12 +3,15 @@
 declare(strict_types=1);
 
 use App\Enums\CatStatus;
+use App\Exceptions\Admin\AdoptionApproveException;
 use App\Models\Cat;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\from;
 use function Pest\Laravel\patch;
+use function Pest\Laravel\withoutExceptionHandling;
 
 it('guest user cannot approve cat adoption', function (): void {
     // Given
@@ -30,6 +33,30 @@ it('basic user cannot approve cat adoption', function (): void {
         ->patch("/admin/cats/{$cat->id}/approve")
         ->assertForbidden();
 });
+
+it('should redirect admin with warning when attempting to cancel adoption of cat with invalid status', function (CatStatus $invalidStatus): void {
+    // Given
+    withoutExceptionHandling();
+
+    Role::create(['name' => 'admin']);
+    $user = User::factory()->create();
+    $user->syncRoles('admin');
+    $cat = Cat::factory()->status($invalidStatus)->create();
+    session(['auth.password_confirmed_at' => time()]);
+
+    // When & Then
+    from('/admin/dashboard')
+        ->actingAs($user)
+        ->patch("/admin/cats/{$cat->id}/cancel")
+        ->assertRedirect('/admin/dashboard')
+        ->assertSessionHas('warning', __('It is possible to approve the adoption of cats that have the status for approval'))
+        ->assertSeeText(__('It is possible to approve the adoption of cats that have the status for approval'));
+
+})->with([
+    'available status' => CatStatus::Available,
+    'adopted status' => CatStatus::Adopted,
+])->throws(AdoptionApproveException::class);
+
 it('admin can approve cat adoption', function (): void {
     // Given
     Role::create(['name' => 'admin']);
@@ -69,7 +96,30 @@ it('basic user cannot cancel cat adoption', function (): void {
         ->assertForbidden();
 });
 
-it('admin can approve cancel adoption', function (): void {
+it('should redirect admin with warning when attempting to approve adoption of cat with invalid status', function (CatStatus $invalidStatus): void {
+    // Given
+    withoutExceptionHandling();
+
+    Role::create(['name' => 'admin']);
+    $user = User::factory()->create();
+    $user->syncRoles('admin');
+    $cat = Cat::factory()->status($invalidStatus)->create();
+    session(['auth.password_confirmed_at' => time()]);
+
+    // When & Then
+    from('/admin/dashboard')
+        ->actingAs($user)
+        ->patch("/admin/cats/{$cat->id}/approve")
+        ->assertRedirect('/admin/dashboard')
+        ->assertSessionHas('warning', __('It is possible to approve the adoption of cats that have the status for approval'))
+        ->assertSeeText(__('It is possible to approve the adoption of cats that have the status for approval'));
+
+})->with([
+    'available status' => CatStatus::Available,
+    'adopted status' => CatStatus::Adopted,
+])->throws(AdoptionApproveException::class);
+
+it('admin can cancel adoption', function (): void {
     // Given
     Role::create(['name' => 'admin']);
     $user = User::factory()->create();
