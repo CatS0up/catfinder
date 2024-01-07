0d54dc13606bc22f3a8e78227d261537ef2af666
Damian <dwoz137@gmail.com>
2024-01-07 20:23:00 +0100

    Create gates for cat management

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\DataObjects\CatData;
+use App\Models\Cat;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,24 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('update-cat', function (User $user, Cat|CatData $catData) {
+            if ($user->hasRole('admin')) {
+                return true;
+            }
 
+            return (bool) ($user->id === $catData->adding_user_id);
+        });
+
+        Gate::define('delete-cat', function (User $user, Cat|CatData $catData) {
+            if ($user->hasRole('admin')) {
+                return true;
+            }
+
+            return (bool) ($user->id === $catData->adding_user_id);
+        });
+
+        Gate::define('choose-cat-for-adoption', fn (User $user) => $user->hasRole('user'));
+
+        Gate::define('approve-cat-adoption', fn (User $user) => $user->hasRole('admin'));
     }
 }
