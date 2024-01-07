255b489350768f83218d5c0324e2102809eb5ec9
Damian <dwoz137@gmail.com>
2024-01-07 23:25:13 +0100

    Create DashboardController

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Admin;
+
+use App\DataTables\Admin\CatsDataTable;
+use App\Http\Controllers\Controller;
+use App\ViewModels\Admin\DashboardViewModel;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    public function index(Request $request): View
+    {
+        return view('admin.dashboard', [
+            'model' => (new DashboardViewModel($request->integer('page', 1)))->toArray(),
+        ]);
+    }
+}
